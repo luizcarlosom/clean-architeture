497479e8774ba63539285cedd60949cff6518507
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-06 21:46:27 -0300

    feat: Connecting project with database and tests

---
+++ src/infra/db/settings/connection.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+class DBConnectionHandler:
+
+    def __init__(self) -> None:
+        self.__connection_string = "{}://{}:{}@{}:{}/{}".format(
+            'mysql+pymysql',
+            'root',
+            '123456',
+            'localhost',
+            '3306',
+            'clean_database'
+        )
+        self.__engine = self.__create_database_engine()
+
+    def __create_database_engine(self):
+        return create_engine(self.__connection_string)
+
+    def get_engine(self):
+        return self.__engine
