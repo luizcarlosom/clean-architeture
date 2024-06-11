60ae64abf72c28c62f33855661741f594ebf3875
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-11 13:16:26 -0300

    refact: Refactoring use case user_finder

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,3 +1,4 @@
+import pytest
 from src.infra.db.tests.users_repository import UsersRepositorySpy
 from .user_finder import UserFinder
 
@@ -13,4 +14,41 @@ def test_find():
 
     assert response["type"] == "Users"
     assert response["count"] == len(response["attributtes"])
-    assert response["attributtes"] != []
+    assert response["attributtes"]
+
+def test_find_error_in_invalid_name():
+    first_name = 'meuNome123'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    with pytest.raises(Exception) as  exc_info:
+        user_finder.find(first_name)
+
+    assert str(exc_info.value) == "Nome invalido para a busca"
+
+def test_find_error_in_long_name():
+    first_name = 'meuNomeEhLuizCarlosOliveiraMaciel'
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    with pytest.raises(Exception) as  exc_info:
+        user_finder.find(first_name)
+
+    assert str(exc_info.value) == "Nome muito grande para busca"
+
+def test_find_error_user_not_found():
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+        
+    first_name = 'meuNome'
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    with pytest.raises(Exception) as  exc_info:
+        user_finder.find(first_name)
+
+    assert str(exc_info.value) == "Usuario nao encontrado"
