068c806aed44f28944326ce389b9598c7a19c96f
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-16 11:51:22 -0300

    feat: Implementing validators

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,8 +1,8 @@
 from src.presentation.http_types.http_response import HttpResponse
-from .types import HttpNotFoundError, HttpBadRequestError
+from .types import HttpNotFoundError, HttpBadRequestError, HttpUnprocessableEntityError
 
 def handle_errors(error: Exception) -> HttpResponse:
-    if isinstance(error, (HttpNotFoundError, HttpBadRequestError)):
+    if isinstance(error, (HttpNotFoundError, HttpBadRequestError, HttpUnprocessableEntityError)):
         return HttpResponse(
             status_code=error.status_code,
             body={
